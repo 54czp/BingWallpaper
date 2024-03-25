91d106d2d072d2c5294fb667c636506f2767b935
54czp <15260117208@163.com>
2024-03-25 23:29:13 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,2 +1,17 @@
-$shell = " -NoLogo -NoProfile -NonInteractive -WindowStyle Hidden $PSScriptRoot\wallup.ps1"
-Register-ScheduledTask BingWallpaper BingWallpaper (New-ScheduledTaskAction powershell $shell ) (New-ScheduledTaskTrigger -Daily -At 22:05)  (New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -RunOnlyIfNetworkAvailable -StartWhenAvailable)
+# 导入所需的模块
+Import-Module ScheduledTasks
+
+# 定义任务的参数
+$taskName = "BingWallpaper"
+$scriptPath = "$PSScriptRoot\wallup.ps1"
+$triggerTime = New-ScheduledTaskTrigger -At 00:00 -Daily
+$triggerAtStartup = New-ScheduledTaskTrigger -AtStartup -RandomDelay (New-TimeSpan -Minutes 1)
+
+# 创建一个新的操作，该操作将执行指定的脚本
+$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-WindowStyle Hidden -File `"$scriptPath`""
+
+# 创建一个新的任务设置，该设置将在用户登录后的一段时间开始任务，并设置任务为隐藏
+$settings = New-ScheduledTaskSettingsSet -StartWhenAvailable -Hidden
+
+# 创建一个新的计划任务，设置任务的用户信息
+Register-ScheduledTask -TaskName $taskName -Trigger $triggerTime, $triggerAtStartup -Action $action -Settings $settings
